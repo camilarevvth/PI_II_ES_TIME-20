23e95a7b964eccef56dd26f0a45e752f003ce9e1
esqueleto098 <guoliveirsantos@gmail.com>
2025-10-16 10:18:37 -0300

    inicialização da conexão sql

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,49 @@
 const express = require('express');
 const cors = require('cors');
+const sql = require('mysql');
 const app = express();
 const port = 3000;
+const conection = sql.createConnection({
+    host: "",
+    user: "",
+    password: "",
+    database: ""
+});
 
-app.use(json());
+app.use(express.json());
 app.use(cors());
 
 app.post('/cadastrarusuario', (req, res) => {
-    const { nome, email, senha } = req.body;
+    const nome = req.body.nome;
+    const email = req.body.email;
+    const celular = req.body.celular;
+    const senha = req.body.senha;
 
-    //postar no banco
+    
 });
 
 app.get('/login', () => {
-    const { email, senha } = req.body;
+    const email = req.body.email;
 
     let existe = false;
 
-    //buscar no banco de dados
+    //buscar
 
-    res.body = json({ existe });
+    res.json({ existe });
 });
 
 app.listen(port, () => {
-    console.log("servidor rodando na porta 3000");
+    console.log(`servidor rodando na porta ${port}`);
+});
+
+//front
+const email = '';
+const senha = '';
+
+const data = await fetch('/cadastro', {
+    method: 'POST',
+    headers: {
+        'content-type': 'application.json'
+    },
+    body: JSON.stringify({email, senha})
 });
